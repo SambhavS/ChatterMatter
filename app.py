4d3be8ea111c714462584c327df138d71b25e5b9
sambhav <sambhav.reddy@gmail.com>
2018-01-11 18:42:36 -0800

    changes

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def deserialize(path):
 #####	
 @app.route('/')
 def hi_there():
-	return "Howdy! To go to a chatroom with a friend, please enter a positive integer into the url after the .com/ ; For example why not go to <a href='https://pure-sea-80295.herokuapp.com'> https://pure-sea-80295.herokuapp.com/100</a>"
+	return "Howdy! To go to a chatroom with a friend, please enter a positive integer into the url after the .com/ ; For example, why not go to <a href='https://pure-sea-80295.herokuapp.com/100'> https://pure-sea-80295.herokuapp.com/100</a>"
 
 @app.route('/<session>')
 def hello_world(session):
@@ -40,10 +40,5 @@ def add(session):
 		serialize(info, path)
 	return json.JSONEncoder().encode(messages)
 
-
-
-
-
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
